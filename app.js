7523f8c2e5e2db8455eb09cd1802c6f8831dda32
Marianne Webb <mvwebb88@hotmail.com>
2026-01-05 14:33:23 -0500

    Complete Exercise 1: apply map() to multiply array values

---
+++ app.js
@@ -0,0 +1,10 @@
+//Exercise 1
+// Starting code:
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// My code here:
+const numsTimesTwo = nums.map((num) => {
+  return num * 2;
+});
+
+console.log(numsTimesTwo);
